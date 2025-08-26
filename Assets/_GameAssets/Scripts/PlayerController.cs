59b2d679520b2f4905b74eca88e1cce15e604509
esats <esat.avsr@gmail.com>
2025-08-26 00:52:20 -0400

    up
    uup

--- Assets/_GameAssets/Scripts/PlayerController.cs
+++ Assets/_GameAssets/Scripts/PlayerController.cs
@@ -3,6 +3,11 @@ using UnityEngine;
 public class NewMonoBehaviourScript : MonoBehaviour
 {
     [SerializeField] private Transform _orientationTransform;
+    [SerializeField] private float _movementSpeed;
+    [SerializeField] private KeyCode _jumpKey;
+    [SerializeField] private float _jumpForce;
+    [SerializeField] private bool _canJump;
+
     private Rigidbody _playerRigidbody;
     private float _horizontalInput, _verticalInput;
     private Vector3 _movementDirection;
@@ -10,6 +15,7 @@ public class NewMonoBehaviourScript : MonoBehaviour
     private void Awake()
     {
         _playerRigidbody = GetComponent<Rigidbody>();
+        _playerRigidbody.freezeRotation = true;
     }
 
     private void Update()
@@ -26,6 +32,12 @@ public class NewMonoBehaviourScript : MonoBehaviour
     {
         _horizontalInput = Input.GetAxis("Horizontal");
         _verticalInput = Input.GetAxis("Vertical");
+
+        if (Input.GetKey(_jumpKey) && _canJump)
+        {
+            _canJump = false;
+            SetPlayerJumping();
+        }
     }
 
     private void SetPlayerMovement()
@@ -33,7 +45,12 @@ public class NewMonoBehaviourScript : MonoBehaviour
         _movementDirection = _orientationTransform.forward * _verticalInput +
                         _orientationTransform.right * _horizontalInput;
 
-        _playerRigidbody.AddForce(_movementDirection * 10f, ForceMode.Force);                     
-        
+        _playerRigidbody.AddForce(_movementDirection.normalized * _movementSpeed, ForceMode.Force);
+    }
+    
+    private void SetPlayerJumping()
+    {
+        _playerRigidbody.linearVelocity = new Vector3(_playerRigidbody.linearVelocity.x, 0f, _playerRigidbody.linearVelocity.z);                     
+        _playerRigidbody.AddForce(transform.up * _jumpForce, ForceMode.Impulse);                     
     }
 }
