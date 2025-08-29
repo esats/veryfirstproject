bb627aab26b6d15e9ceeacbb13ca2951e99596b2
esats <esat.avsr@gmail.com>
2025-08-29 00:26:48 -0400

    up

--- Assets/_GameAssets/Scripts/Camera/ThirdPersonCameraController.cs
+++ Assets/_GameAssets/Scripts/Camera/ThirdPersonCameraController.cs
@@ -1,6 +1,31 @@
- using UnityEngine;
+using UnityEngine;
 
 public class ThirdPersonCamera : MonoBehaviour
 {
-   
+    [Header("References")]
+    [SerializeField] private Transform _playerTransform;
+    [SerializeField] private Transform _orientationTransform;
+    [SerializeField] private Transform _playerVisualTransform;
+
+    [Header("Settings")]
+    [SerializeField] private float _rotationSpeed = 10f;
+
+    private void Update()
+    {
+        Vector3 viewDirection = _playerTransform.position - new Vector3(transform.position.x, _playerTransform.position.y, transform.position.z);
+
+        if (viewDirection.sqrMagnitude > 0.0001f)
+            _orientationTransform.forward = viewDirection.normalized;
+
+        float horizontalInput = Input.GetAxisRaw("Horizontal");
+        float verticalInput = Input.GetAxisRaw("Vertical");
+
+        Vector3 inputDirection = _orientationTransform.forward * verticalInput + _orientationTransform.right * horizontalInput;
+
+        if (inputDirection.sqrMagnitude > 0.0001f)
+        {
+            Quaternion targetRot = Quaternion.LookRotation(inputDirection.normalized, Vector3.up);
+            _playerVisualTransform.rotation = Quaternion.Slerp(_playerVisualTransform.rotation, targetRot, Time.deltaTime * _rotationSpeed);
+        }
+    }
 }
