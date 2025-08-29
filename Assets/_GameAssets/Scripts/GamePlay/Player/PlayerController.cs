db7a72979b97dc7a8add14f3c65da56e94226dcc
esats <esat.avsr@gmail.com>
2025-08-29 00:10:44 -0400

    up up
    up

--- Assets/_GameAssets/Scripts/GamePlay/Player/PlayerController.cs
+++ Assets/_GameAssets/Scripts/GamePlay/Player/PlayerController.cs
@@ -1,15 +1,31 @@
+using System;
 using UnityEngine;
 
 public class NewMonoBehaviourScript : MonoBehaviour
 {
+    [Header("References")]
     [SerializeField] private Transform _orientationTransform;
+
+    [Header("Movement Settings")]
     [SerializeField] private float _movementSpeed;
+    [SerializeField] private KeyCode _movementKey;
+
+    [Header("Jump Settings")]
     [SerializeField] private KeyCode _jumpKey;
     [SerializeField] private float _jumpForce;
-    [SerializeField] private bool _canJump;
     [SerializeField] private float _jumpCoolDown;
+    [SerializeField] private bool _canJump;
+
+    [Header("Sliding Settings")]
+    [SerializeField] private KeyCode _slideKey;
+    [SerializeField] private float _slideMultiplier;
+    [SerializeField] private float _slideDrag;
+    [SerializeField] private bool _isSliding;
+
+    [Header("Ground Check Settings")]
     [SerializeField] private float _playerHeight;
     [SerializeField] private LayerMask _groundLayer;
+    [SerializeField] private float _groundDrag;
 
     private Rigidbody _playerRigidbody;
     private float _horizontalInput, _verticalInput;
@@ -24,6 +40,8 @@ public class NewMonoBehaviourScript : MonoBehaviour
     private void Update()
     {
         SetInputs();
+        SetPlayerDrag();
+        LimitPlayerSpeed();
     }
 
     private void FixedUpdate()
@@ -36,7 +54,15 @@ public class NewMonoBehaviourScript : MonoBehaviour
         _horizontalInput = Input.GetAxis("Horizontal");
         _verticalInput = Input.GetAxis("Vertical");
 
-        if (Input.GetKey(_jumpKey) && _canJump && IsGrounded())
+        if (Input.GetKeyDown(_slideKey))
+        {
+            _isSliding = true;
+        }
+        else if (Input.GetKeyDown(_movementKey))
+        {
+            _isSliding = false;
+        }
+        else if (Input.GetKey(_jumpKey) && _canJump && IsGrounded())
         {
             _canJump = false;
             SetPlayerJumping();
@@ -49,13 +75,43 @@ public class NewMonoBehaviourScript : MonoBehaviour
         _movementDirection = _orientationTransform.forward * _verticalInput +
                         _orientationTransform.right * _horizontalInput;
 
-        _playerRigidbody.AddForce(_movementDirection.normalized * _movementSpeed, ForceMode.Force);
+        if (_isSliding)
+        {
+            _playerRigidbody.AddForce(_movementDirection.normalized * _movementSpeed * _slideMultiplier, ForceMode.Force);
+        }
+        else
+        {
+            _playerRigidbody.AddForce(_movementDirection.normalized * _movementSpeed, ForceMode.Force);
+        }
+    }
+
+    private void SetPlayerDrag()
+    {
+        if (_isSliding)
+        {
+            _playerRigidbody.linearDamping = _slideDrag;
+        }
+        else
+        {
+            _playerRigidbody.linearDamping = _groundDrag;
+        }
     }
 
     private void SetPlayerJumping()
     {
         _playerRigidbody.linearVelocity = new Vector3(_playerRigidbody.linearVelocity.x, 0f, _playerRigidbody.linearVelocity.z);
         _playerRigidbody.AddForce(transform.up * _jumpForce, ForceMode.Impulse);
+    }
+
+    private void LimitPlayerSpeed()
+    {
+        Vector3 flatVelocity = new Vector3(_playerRigidbody.linearVelocity.x, 0f, _playerRigidbody.linearVelocity.z);
+
+        if (flatVelocity.magnitude > _movementSpeed)
+        {
+            Vector3 limitedVelocity = flatVelocity.normalized * _movementSpeed;
+            _playerRigidbody.linearVelocity = new Vector3(limitedVelocity.x, limitedVelocity.y, limitedVelocity.z);
+        }
     }
 
     private void ResetJumping()
